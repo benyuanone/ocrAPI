3a93b1beaa1c8ff88b4f0940a29f417b519cf865
chengang <10740128852qq.com>
2017-12-26 15:07:17 +0800

    身份证识别、驾驶证、合同、行驶证

--- web-service/src/main/java/com/ourway/baiduapi/action/BaiDuApi.java
+++ web-service/src/main/java/com/ourway/baiduapi/action/BaiDuApi.java
@@ -18,10 +18,16 @@ import java.util.Map;
  * Created by D.chen.g on 2017/12/25.
  */
 public class BaiDuApi {
-    public final static String API_TOKEN="24.4ef7a1ae0613cc6abccdf21ae744f869.2592000.1516796619.282335-10580248";
-    public static final String APP_ID = "10580248";
-    public static final String API_KEY = "aK9ba9d59VCIiscWXMaO8SPX";
-    public static final String SECRET_KEY = "0p0xZnrZWoEZQwv7NdVh5YBVm41C3FoZ";
+    private static final String ACCESS_TOKEN = "access_token";
+    /**
+    *<p>方法:getToKen TODO 获取token</p>
+    *<ul>
+     *<li> @param  TODO</li>
+    *<li>@return void  </li>
+    *<li>@author D.cehn.g </li>
+    *<li>@date 2017/12/26 15:06  </li>
+    *</ul>
+    */
     public static void getToKen(){
         String access_token_url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials"
                 +"&client_id="+ BaiDuApiInfo.API_KEY +"&client_secret="+BaiDuApiInfo.SECRET_KEY;
@@ -29,8 +35,8 @@ public class BaiDuApi {
         CloseableHttpResponse response =  HttpClientUtils.doHttpsGet(access_token_url,null);
         String result=HttpClientUtils.toString(response);
         Map<String,Object> tokenMap=JsonUtil.jsonToMap(result);
-        if(null!=tokenMap&& !TextUtils.isEmpty(tokenMap.get("access_token"))){
-            BaiDuApiInfo.TOKEN=tokenMap.get("access_token").toString();
+        if(null!=tokenMap&& !TextUtils.isEmpty(tokenMap.get(ACCESS_TOKEN))){
+            BaiDuApiInfo.TOKEN=tokenMap.get(ACCESS_TOKEN).toString();
         }
     }
 
@@ -44,9 +50,9 @@ public class BaiDuApi {
     *<li>@date 2017/12/26 13:57  </li>
     *</ul>
     */
-    public static List<ValueDTO> IdCardDiscriminate(String filePath){
+    public static List<ValueDTO> idCardDiscriminate(String filePath){
         String image = Base64ImageUtils.GetImageStrFromPath(filePath);
-        String url = "https://aip.baidubce.com/rest/2.0/ocr/v1/idcard?access_token="+API_TOKEN;
+        String url = "https://aip.baidubce.com/rest/2.0/ocr/v1/idcard?access_token="+BaiDuApiInfo.TOKEN;
         Map<String, String> headers = new HashMap<String, String>();
         headers.put("Content-Type", "application/x-www-form-urlencoded");
         Map<String, String> bodys = new HashMap<String, String>();
@@ -72,13 +78,120 @@ public class BaiDuApi {
 
     }
 
+
+    /**
+     *<p>方法:generalDiscriminate TODO 通用文字识别</p>
+     *<ul>
+     *<li> @param filePath TODO图片文件路径</li>
+     *<li>@return java.util.List  </li>
+     *<li>@author D.cehn.g </li>
+     *<li>@date 2017/12/26 13:57  </li>
+     *</ul>
+     */
+    public static List generalDiscriminate(String filePath){
+        String image = Base64ImageUtils.GetImageStrFromPath(filePath);
+        String url = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token="+BaiDuApiInfo.TOKEN;
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/x-www-form-urlencoded");
+        Map<String, String> bodys = new HashMap<String, String>();
+        bodys.put("image", image);
+        bodys.put("language_type", "CHN_ENG");
+        bodys.put("detect_language", "true");
+        bodys.put("probability", "false");
+        try {
+            CloseableHttpResponse response =  HttpClientUtils.doHttpsPost(url,headers,bodys);
+            String result=HttpClientUtils.toString(response);
+            if(!TextUtils.isEmpty(result)){
+                Map idcardDTO=JsonUtil.jsonToMap(result);
+                List vat= (List)idcardDTO.get(ValueUtils.WORDS_RESULT);
+                return vat;
+            }else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+    *<p>方法:driverDiscriminate TODO 驾驶证识别</p>
+    *<ul>
+     *<li> @param filePath TODO</li>
+    *<li>@return java.util.List<com.ourway.baiduapi.dto.ValueDTO>  </li>
+    *<li>@author D.cehn.g </li>
+    *<li>@date 2017/12/26 14:48  </li>
+    *</ul>
+    */
+    public static List<ValueDTO> driverDiscriminate(String filePath){
+        String image = Base64ImageUtils.GetImageStrFromPath(filePath);
+        String url = "https://aip.baidubce.com/rest/2.0/ocr/v1/driving_license?access_token="+BaiDuApiInfo.TOKEN;
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/x-www-form-urlencoded");
+        Map<String, String> bodys = new HashMap<String, String>();
+        bodys.put("image", image);
+        bodys.put("detect_direction", "true");
+        bodys.put("unified_valid_period", "true");
+        try {
+            CloseableHttpResponse response =  HttpClientUtils.doHttpsPost(url,headers,bodys);
+            String result=HttpClientUtils.toString(response);
+            if(!TextUtils.isEmpty(result)){
+                Map idcardDTO=JsonUtil.jsonToMap(result);
+                Map<String,Object> value= (Map<String,Object>)idcardDTO.get(ValueUtils.WORDS_RESULT);
+                List<ValueDTO> valueDTOS=ValueUtils.getDriverCardValue(value);
+                return valueDTOS;
+            }else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+    *<p>方法:vechiceDiscriminate TODO 行驶证识别</p>
+    *<ul>
+     *<li> @param filePath TODO</li>
+    *<li>@return java.util.List<com.ourway.baiduapi.dto.ValueDTO>  </li>
+    *<li>@author D.cehn.g </li>
+    *<li>@date 2017/12/26 14:51  </li>
+    *</ul>
+    */
+    public static List<ValueDTO> vechiceDiscriminate(String filePath){
+        String image = Base64ImageUtils.GetImageStrFromPath(filePath);
+        String url = "https://aip.baidubce.com/rest/2.0/ocr/v1/vehicle_license?access_token="+BaiDuApiInfo.TOKEN;
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/x-www-form-urlencoded");
+        Map<String, String> bodys = new HashMap<String, String>();
+        bodys.put("image", image);
+        bodys.put("detect_direction", "true");
+        bodys.put("accuracy", "normal");
+        try {
+            CloseableHttpResponse response =  HttpClientUtils.doHttpsPost(url,headers,bodys);
+            String result=HttpClientUtils.toString(response);
+            if(!TextUtils.isEmpty(result)){
+                Map idcardDTO=JsonUtil.jsonToMap(result);
+                Map<String,Object> value= (Map<String,Object>)idcardDTO.get(ValueUtils.WORDS_RESULT);
+                List<ValueDTO> valueDTOS=ValueUtils.getDriverCardValue(value);
+                return valueDTOS;
+            }else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     public static void main(String[] args) {
-        List list=IdCardDiscriminate("D:/xinfeng/1.png");
+        List list=vechiceDiscriminate("D:/xinfeng/5.jpg");//("D:/xinfeng/1.png");
         for(Object o:list){
             ValueDTO v=(ValueDTO)o;
             System.out.println(v.getKey()+":"+v.getWords());
         }
-       // getToKenTest();//先获取token，再替换掉现有写的
+       // driverDiscriminate("D:/xinfeng/4.png");
+//       getToKen();//先获取token，再替换掉现有写的
     }
 
 }
--- web-service/src/main/java/com/ourway/baiduapi/utils/ValueUtils.java
+++ web-service/src/main/java/com/ourway/baiduapi/utils/ValueUtils.java
@@ -14,6 +14,16 @@ import java.util.Set;
  */
 public class ValueUtils {
     private final static String WORDS="words";
+    public final static String WORDS_RESULT="words_result";
+    /**
+    *<p>方法:getIdCardValue TODO 处理身份证识别结果</p>
+    *<ul>
+     *<li> @param params TODO</li>
+    *<li>@return java.util.List<com.ourway.baiduapi.dto.ValueDTO>  </li>
+    *<li>@author D.cehn.g </li>
+    *<li>@date 2017/12/26 14:48  </li>
+    *</ul>
+    */
     public static List<ValueDTO> getIdCardValue(Map<String, MorphDynaBean> params) throws Exception{
         if (null == params) {
             return null;
@@ -25,6 +35,36 @@ public class ValueUtils {
         for (String key : keys) {
             map = params.get(key);
             if (null!=map) {
+                valueDTO = new ValueDTO();
+                valueDTO.setKey(key);
+                valueDTO.setWords(map.get(WORDS).toString());
+                valueDTOList.add(valueDTO);
+            }
+        }
+        return valueDTOList;
+    }
+
+    /**
+    *<p>方法:getDriverCardValue TODO 识别驾驶证</p>
+    *<ul>
+     *<li> @param params TODO</li>
+    *<li>@return java.util.List<com.ourway.baiduapi.dto.ValueDTO>  </li>
+    *<li>@author D.cehn.g </li>
+    *<li>@date 2017/12/26 14:49  </li>
+    *</ul>
+    */
+    public static List<ValueDTO> getDriverCardValue(Map<String, Object> params) throws Exception{
+        if (null == params) {
+            return null;
+        }
+        Set<String> keys = params.keySet();
+        List<ValueDTO> valueDTOList = new ArrayList<>(keys.size());
+        ValueDTO valueDTO = null;
+        Map<String,Object> map = null;
+        for (String key : keys) {
+            Object object = params.get(key);
+            if (null!=object) {
+                map=(Map<String, Object>)object;
                 valueDTO = new ValueDTO();
                 valueDTO.setKey(key);
                 valueDTO.setWords(map.get(WORDS).toString());
